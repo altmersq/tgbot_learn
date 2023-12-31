fe6548ed158af6feaee787e96566a37a7729fc16
Aleks <aleksandro551@bk.ru>
2023-12-31 15:08:43 +0300

    replying

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import os
 import requests
+import telebot
 
+
+with open('token.txt', 'r') as file:
+    token = file.read().strip()
+
+# Инициализация бота
+bot = telebot.TeleBot(token)
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.reply_to(message, "Готов!")
+
+# Обработчик всех сообщений пользователя
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+# Запуск бота
+bot.polling()
